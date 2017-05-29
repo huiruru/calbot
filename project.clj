ad69ed58d487f1588b76f75c0e53b77e2dd75299
huiruru <huiru@chartbeat.com>
2017-05-29 17:15:19 -0400

    Add initial project dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject calbot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "initially a commandline tool to talk to google calendar"
+  :url "http://github.com/huiruru/calbot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [google-apps-clj "0.6.1"]
+                 [clj-time "0.13.0"]]
   :main ^:skip-aot calbot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
