b629970f7f77013438160f65c1f695fdd7d73b71
huiruru <huiru@chartbeat.com>
2017-05-29 17:01:10 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to calbot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/calbot/core_test.clj
@@ -0,0 +1,7 @@
+(ns calbot.core-test
+  (:require [clojure.test :refer :all]
+            [calbot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
